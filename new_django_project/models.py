ce4a4fd2da94a821a05e298d327c058068c0c536
ViolOdn <veta0205@mail.ru>
2019-02-12 19:26:02 +0300

    Third commit

--- new_django_project/models.py
+++ new_django_project/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime
 
-
+'''
 class Family(models.Model):
-    #family_id = models.object.id
     family_id = models.IntegerField(default=1)
     family_name = models.ForeignKey(User, on_delete=models.CASCADE)
     family_email = models.CharField(max_length=100, default="")
@@ -11,32 +11,34 @@ class Family(models.Model):
 
     def __repr__(self):
         return self.family_name + ' ' + self.password_hash
+'''
 
-
+'''
 class FamilyMembers(models.Model):
-    family_id = models.ForeignKey(Family, on_delete=models.CASCADE, default=1)
-    family_member_id = models.IntegerField(blank=True)
+    #family_member_id = models.IntegerField(blank=True)
+    family_id = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     family_member_name = models.CharField(max_length=100)
     family_member_description = models.CharField(max_length=100)
-
+'''
 
 class Categories(models.Model):
-    family_id = models.ForeignKey(Family, on_delete=models.CASCADE, default=1)
-    #type_id = models.IntegerField(default=1)
+    family_id = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    type_id = models.IntegerField(default=1)
     type_name = models.CharField(max_length=50)
 
 
 class FlowOfFunds(models.Model):
-    family_member_id = models.ForeignKey(Family, on_delete=models.CASCADE, default=1)
+    family_id = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    #family_member_id = models.ForeignKey(FamilyMembers, on_delete=models.CASCADE, default=1)
     sum = models.FloatField(default=0)
     type_id = models.ForeignKey(Categories, on_delete=models.CASCADE, default=1)
     description = models.CharField(max_length=100)
-    date = models.DateField()
+    date = models.DateField(default=datetime.date(datetime.today()))
     is_it_expense = models.BooleanField(default=True)
 
 
 class ExpensesPlan(models.Model):
-    family_id = models.ForeignKey(Family, on_delete=models.CASCADE, default=1)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     type_id = models.ForeignKey(Categories, on_delete=models.CASCADE, default=1)
     sum_plan = models.FloatField(default=0)
     start_date = models.DateField()
